c7f74314d9b8f347161ea7d8244c0737d0804bce
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-13 18:58:19 +0200

    feat: cloudinary file for image upload

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,14 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const cloudinary = require("cloudinary").v2;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.COLUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export { cloudinary };
