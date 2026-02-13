132c930edfa95b0deeedca7ff37046a6e7d26722
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-13 18:31:13 +0200

    feat: added swagger for documenting api endpoints better and instaleld morgan for advanced logging

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import fileupload from "express-fileupload";
+import cors from "cors";
+import morgan from "morgan";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
+//middlewares
 const app = express();
+app.use(morgan("dev"));
 
+app.use(cors());
 app.use(express.json());
+app.use(fileupload({ useTempFiles: true }));
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Clinic App API",
+      version: "1.0.0",
+      description: "API documentation for the Clinic app backend",
+    },
+  },
+  apis: ["./routes/*.js"], // where your route files are
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export default app;
