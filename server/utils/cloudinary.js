ccb79b82ed076b7803f6cc3b300d5a5cf67cd01b
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-16 07:58:44 +0200

    fix:fixed typo error thatw as causing the api key to go missing

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -7,7 +7,7 @@ dotenv.config();
 console.log("CI KEY:", process.env.CLOUDINARY_API_KEY);
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.COLUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
