c28bfd96970cdf73ea7d820d1062ca6416989497
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-18 14:18:33 +0200

    Add new auth interfaces for login, email verification, and password reset flows

--- server/types/auth.type.ts
+++ server/types/auth.type.ts
@@ -22,3 +22,23 @@ export interface RegisterBody {
 export interface VerifyEmailRequestBody {
   email?: string;
 }
+
+export interface LoginBody {
+  email?: string;
+  password?: string;
+}
+
+export interface VerifyEmailQuery {
+  email?: string;
+  token?: string;
+}
+
+export interface ForgotPasswordLinkBody {
+  email?: string;
+  token?: string;
+}
+
+export interface ResetPasswordBody {
+  email?: string;
+  password?: string;
+}
