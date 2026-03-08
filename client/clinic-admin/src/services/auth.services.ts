96e381c48627d8dbd8b60041f4f191af09fae98d
siyabonga <siyabongasamkelociam@gmail.com>
2026-03-08 16:26:41 +0200

    feat: added auth functions to the services

--- client/clinic-admin/src/services/auth.services.ts
+++ client/clinic-admin/src/services/auth.services.ts
@@ -1,5 +1,9 @@
 import api from "../api/axios.config";
-import type { LoginCredentials, AuthResponse } from "../types/auth.type";
+import type {
+  LoginCredentials,
+  AuthResponse,
+  VerifyEmailRequest,
+} from "../types/auth.type";
 
 export const AuthService = {
   login: async (credentials: LoginCredentials): Promise<AuthResponse> => {
@@ -29,6 +33,79 @@ export const AuthService = {
     } catch (error) {
       console.error("Register failed:", error);
       throw error; // Let the UI handle the error message
+    }
+  },
+
+  verifyEmailRequest: async (
+    credentials: VerifyEmailRequest,
+  ): Promise<AuthResponse> => {
+    try {
+      const response = await api.post(
+        "/auth/verify-email-request",
+        credentials,
+      );
+
+      if (
+        response.data.message ===
+        "If an account exists for this email, a verification link has been sent."
+      ) {
+        console.log("Email verification request successfully sent");
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Email verification request failed:", error);
+      throw error; // Let the UI handle the error message
+    }
+  },
+
+  verifyEmail: async (
+    credentials: VerifyEmailRequest,
+  ): Promise<AuthResponse> => {
+    try {
+      const response = await api.post("/auth/forgot-password", credentials);
+
+      if (response.data.message === "Email successfully verified.") {
+        console.log("Email verified successfully");
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Email verification failed:", error);
+      throw error;
+    }
+  },
+
+  forgotPassword: async (
+    credentials: VerifyEmailRequest,
+  ): Promise<AuthResponse> => {
+    try {
+      const response = await api.post("/auth/forgot-password", credentials);
+
+      if (response.data.message === "Reset link sent to email.") {
+        console.log("Reset link sent to email.");
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Failed to create reset link.", error);
+      throw error;
+    }
+  },
+
+  resetPassword: async (
+    credentials: LoginCredentials,
+  ): Promise<AuthResponse> => {
+    try {
+      const response = await api.post("/auth/reset-password", credentials);
+
+      if (
+        response.data.message ===
+        "Password updated successfully. You can now log in."
+      ) {
+        console.log("Password updated successfully. You can now log in.");
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Failed to reset password", error);
+      throw error;
     }
   },
 
